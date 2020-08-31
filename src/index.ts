d8c2a55ae5a4a6648f262d178af4ec600d9a3ed2
philippmossier <mossier.dev@gmail.com>
2020-08-30 17:01:49 -0700

    add example to create a new post

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,13 @@ import { createConnection } from "typeorm";
 
 const main = async () => {
   await createConnection();
+  // // create new record on Post table
+  // const db_init = await createConnection();
+  // const repository = await db_init.getRepository(Post);
+  // let post = new Post;
+  // post.title = "jajaasd"
+  // await repository.save(post)
+
   const app = express();
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
